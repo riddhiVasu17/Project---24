9b58004ac0f0f126b6b103c3705cc01c4945eb11
Riddhi Vasu <69344418+riddhiVasu17@users.noreply.github.com>
2020-10-03 22:25:40 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,25 @@
+class paper {
+
+    constructor(x,y,radius){
+
+        var option = {
+            restitution : 0.3,
+            friction : 0.5,
+            density : 1.2
+        }
+        this.body = Bodies.circle(x, y, radius, option);
+        World.add(world, this.body);
+
+        this.body.radius = radius;
+
+    }
+
+    display(){
+        var pos = this.body.position;
+    
+        fill("white");
+        ellipseMode(CENTER);
+        circle(pos.x, pos.y, this.body.radius);
+    }
+
+}
